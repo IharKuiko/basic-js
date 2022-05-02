1ec2a81f6e576a8f64bb1301c79549e05ed733c8
IharKuiko <coooll81@gmail.com>
2022-05-02 21:05:06 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++){
+      if (matrix[i][j] === '^^') count++
+    }
+  }
+  return count;
 }
+
+// console.log()
 
 module.exports = {
   countCats
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+members = ['Matt', 'Ann', 'Dmitry', 'Max'];
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let name = [];
+  let strName = '';
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        members[i] = members[i].trim();
+        name.push(members[i][0].toUpperCase());
+      }
+    }
+    name.sort();
+    for (let i = 0; i < name.length; i++){
+      strName += name[i]
+    }
+    // if (strName === '') return false
+    if (strName === '') {
+      return false;
+    } else {
+      return strName;
+    }
+  } else {
+    return false;
+  }
+  
+  // console.log(strName)
 }
 
+// createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']);
 module.exports = {
   createDreamTeam
 };
